729f2b3dc1041b22099639da15e36fb2f32e9b1c
Sreekanth V K <sreekanth.vk@outlook.com>
2021-01-18 22:46:43 +0530

    Updated the code
    Some more examples

--- code/introductionR.R
+++ code/introductionR.R
@@ -5,7 +5,8 @@
 #----------------------------------------------------
 # this is a comment. 
 
-#to know the working directory
+# Select and then Ctrl + Enter
+# to know the working directory
 getwd()
 
 #setting working directory
@@ -41,7 +42,7 @@ variable1 + 1
 variable3 = 1
 variable4 <- 2
 result  <- variable3 + variable4
-
+1:10
 list.var <- 1:10
 list.var <- c(1:10)
 list.var <- seq(1,10,2)
@@ -53,7 +54,7 @@ is.numeric(1L)
 is.integer(1L)
 
 #list function
-list_var <- list(1:10)
+list_var <- list(x= 1:10, y = LETTERS)
 names(list_var) <- "x"
 list_var$x
 list_var["x"]
@@ -75,7 +76,7 @@ df <- data.frame(x=1:10, y = letters[1:10], LETTERS[1:10])
 class(df)
 typeof(df)
 
-sum<- sum(1:10)
+sum <- sum(1:10)
 ?sum
 class(sum)
 rm("sum")
@@ -100,11 +101,14 @@ getmode <- function(value) {
   uniqv[which.max(tabulate(match(value, uniqv)))]
 }
 
+fun.name <- function(arg1, arg2) {
+  arg1 + arg2 }
+fun.name(22,33)
 # Create the vector with numbers.
 list.of.nums <- c(2,1,2,3,1,2,3,4,1,5,5,3,2,3)
 
 # Calculate the mode using the user function.
-result <- getmode(v)
+result <- getmode(list.of.nums)
 print(result)
 
 num <- readline("Enter a number:")
@@ -122,13 +126,13 @@ if (num > 0)
 }
   
 # switch statement
-switch(1,sum(1:10), mean(1:10), median(1:10))
+switch(2,sum(1:10), mean(1:10), median(1:10))
 
 #for loop
 for (i in 1:10) {
   print(i)
 }
-
+letters
 for (i in LETTERS) {
   print(i)
 }
@@ -155,7 +159,7 @@ vapply(x, mean, "a")   # it will throw error
 # range
 (r.x <- range(stats::rnorm(100)))
 diff(r.x) # the SAMPLE range
-
+installed.packages()
 install.packages("ggplot2")
 
 library(ggplot2)
@@ -169,14 +173,17 @@ require(crantastic)
 sample.data <- read.csv("data/sample.csv")
 sample.data <- read.table("data/sample.csv", sep = ",", header = T)
 sample.data <- read.delim("data/sample.csv", sep = ",", header = T)
+
 library(readxl)
 sample.datax <- read_excel("data/sample.xlsx")
 
-write.csv(sample.data, "data/sample.csv")
+write.csv(sample.data, file = "data/sample.csv")
+write.csv(sample.datax, "data/sample.csv")
+
 read.csv
 read.csv2
 
-install.packages(c("sp", "rgdal"))
+# install.packages(c("sp", "rgdal"))
 library(sp)
 library(rgdal)
 
@@ -186,3 +193,14 @@ plot(neighborhood)
 help("datasets")
 library(help = "datasets")
 library(help = "ISLR")
+
+help("read.csv")
+
+?read.csv
+
+citation()
+citation("datasets")
+
+cat("\014") 
+ls()
+rm(list = ls())
